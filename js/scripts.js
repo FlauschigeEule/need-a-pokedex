f2261e3dcec15a9b02532310857584c4df0375d1
FlauschigeEule <juliane.lallinger@gmail.com>
2022-10-04 12:51:43 -0700

    Create base for pokedex app

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+alert('Hello world!');
+
+let myName = 'Bob';
+document.write(myName); 
+document.write('<br>');
+
+myName = 'Julie';
+document.write(myName);
+document.write('<br>');
+
+let mountain = 1860;
+let biggerMountain = mountain * 2;
+document.write(biggerMountain);
+document.write('<br>');
+
+let icecream = 1.80;
+let fiveIcecream = (icecream * 10) / 2;
+document.write(fiveIcecream);
+document.write('<br>');
+
+let myFavFood = 'My favorite food is authentic Italian Pizza';
+document.write(myFavFood);
