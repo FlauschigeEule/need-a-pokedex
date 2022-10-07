113f1bd2d3a27a57120d4e7d70eb38fe47368988
FlauschigeEule <juliane.lallinger@gmail.com>
2022-10-07 07:39:11 -0700

    Update scripts.js to create pokemonList
    Delete former JS practice and create a pokemonList for the Pokedex app

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,74 @@
-alert('Hello world!');
-
-let myName = 'Bob';
-document.write(myName); 
-document.write('<br>');
-
-myName = 'Julie';
-document.write(myName);
-document.write('<br>');
-
-let mountain = 1860;
-let biggerMountain = mountain * 2;
-document.write(biggerMountain);
-document.write('<br>');
-
-let icecream = 1.80;
-let fiveIcecream = (icecream * 10) / 2;
-document.write(fiveIcecream);
-document.write('<br>');
-
-let myFavFood = 'My favorite food is authentic Italian Pizza';
-document.write(myFavFood);
+let pokemonList = [
+    {
+        name: 'Balbasaur',
+        types: ['grass', 'fire'],
+        height: 0.7,
+        weight: 6.9
+    },
+    {
+        name: 'Charmander',
+        types: 'fire',
+        height: 0.6,
+        weight: 8.5
+    },
+    {
+        name: 'Eevee',
+        types: 'normal',
+        height: 0.3,
+        weight: 6.5
+    },
+    {
+        name: 'Growlithe',
+        types: 'fire',
+        height: 0.7,
+        weight: 19
+    },
+    {
+        name: 'Hoothoot',
+        types: ['flying', 'normal'],
+        height: 0.7,
+        weight: 21.2
+    },
+    {
+        name: 'Nidoran',
+        types: 'poison',
+        height: 0.4,
+        weight: 7
+    },
+    {
+        name: 'Pikachu',
+        types: 'electric',
+        height: 0.4,
+        weight: 6
+    }, 
+    {
+        name: 'Ponyta',
+        types: 'fire',
+        height: 1,
+        weight: 30
+    },
+    {
+        name: 'Psyduck',
+        types: 'water',
+        height: 0.8,
+        weight: 19.6
+    },
+    {
+        name: 'Sandshrew',
+        types: 'ground',
+        height: 0.6,
+        weight: 12
+    },
+    {
+        name: 'Squirtle',
+        types: 'water',
+        height: 0.5,
+        weight: 9
+    },
+    {
+        name: 'Vulpix',
+        types: 'fire',
+        height: 0.6,
+        weight: 9.9
+    }
+];
